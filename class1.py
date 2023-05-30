23aee2403cadc2539f6ffa1bcc61fb24c5879f60
klim312 <klim.papin@mail.ru>
2023-05-30 17:11:27 +0600

    class1

--- class1.py
+++ class1.py
@@ -1,3 +1,4 @@
 class Hello:
     def __init__(self, str):
         self.str = str
+class Myclass(Hello):pass
--- task.py
+++ task.py
@@ -1,2 +1,7 @@
 from art import tprint
 tprint("PYTHON")
+
+from class1 import MyClass
+
+obj = MyClass()
+obj.tptint()
